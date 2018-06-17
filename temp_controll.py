84a708d82d166a33bdd7509ca5bf211f1eed8fc0
Rasmus Koit <rasmuskoit@gmail.com>
2018-06-17 20:44:58 +0300

    Added sleep timer to make sure loop doesn't run too often.

--- temp_controll.py
+++ temp_controll.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 from subprocess import check_output, call
-
+from time import sleep
 while True:
+    sleep(30)
     value = float(str(check_output(["/opt/vc/bin/vcgencmd", "measure_temp"]))[7:-5])
 
     if (value > 60):
